684116074198811e29523653e5bba6ec24576905
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-04-21 18:05:36 -0400

    added language descriptions

---
+++ src/utilities/getSupportedLanguages.js
@@ -0,0 +1,7 @@
+const getSupportedLanguages = () => {
+    return [
+    "ar", "bg", "ca", "de", "el", "en", "es", "fa", "fi", "fr", "gl", "he", "hi",
+    "id", "it", "ja", "la", "lt", "pt", "ru", "sr", "th", "tr", "vi", "zu"
+]
+}
+export default getSupportedLanguages
